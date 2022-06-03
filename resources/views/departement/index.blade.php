3a4f0f4109a1e6134d4db33a4d1a09ee75e37751
Weldhan Seftian Hasfa <weldhanhasfa8@gmail.com>
2022-06-03 14:44:59 +0700

    add button tambah

--- resources/views/departement/index.blade.php
+++ resources/views/departement/index.blade.php
@@ -2,7 +2,14 @@
     <div class="col-12">
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Data Departement</h3>
+            <h3 class="card-title">Data Departement</h3>
+            <div class="card-actions">
+                <a href="#" class="btn btn-primary">
+                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                Tambah Data
+                </a>
+            </div>
         </div>
         <div class="card-body">
             <div class="table-responsive">
@@ -31,7 +38,7 @@
                 </table>
             </div>
         </div>
-      </div>
+    </div>
     </div>
 
     @push('extra-styles')
